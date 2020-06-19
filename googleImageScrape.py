fb1e8317af1ba5947410f62d4b64cfc760622077
sebastianalegre <salegre.morris@gmail.com>
2020-06-19 10:11:43 -0500

    Mod Readme

--- googleImageScrape.py
+++ googleImageScrape.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
-import os
 
 # This is the input for your search term
 searchterm = input('What are you searching for? ')
 searchurl = 'urls_' + searchterm + '.txt'
 
-url = "https://www.google.co.in/search?q="+searchterm+"&source=lnms&tbm=isch"
-
 # Modify path to chromedriver
-browser = webdriver.Chrome('/Users/sebastianalegre/Documents/Drivers/chromedriver')
+browser = webdriver.Chrome('{/your_path_to}/chromedriver')
 
+url = "https://www.google.co.in/search?q="+searchterm+"&source=lnms&tbm=isch"
 browser.get(url)
 extensions = { "jpg", "jpeg", "png", "gif" }
 
@@ -21,7 +19,6 @@ imges = []
 for i in html:
     if i.startswith('http') and i.split('"')[0].split('.')[-1] in extensions:
         imges.append(i.split('"')[0])
-print(imges)
 
 with open(searchurl, 'w') as f:
     for item in imges:
